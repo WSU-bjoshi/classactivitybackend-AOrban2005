5558e720c561249c4ea02d454dd54384a0f54d71
AOrban2005 <aorban2005@gmail.com>
2026-02-20 14:03:59 -0500

    Feb 20 class

--- database.sql
+++ database.sql
@@ -1,12 +1,14 @@
 CREATE DATABASE todo_db;
 
 
-USE todo_do;
+USE todo_db;
 
--- CREATE TABLE todos(
--- id INT AUTO_INCREMENT PRIMARY KEY,
--- task VARCHAR(255),
--- completed BOOLEAN DEFAULT FALSE);
+CREATE TABLE todos(
+id INT AUTO_INCREMENT PRIMARY KEY,
+task VARCHAR(255),
+
+completed BOOLEAN DEFAULT FALSE,
+in_use BOOLEAN DEFAULT TRUE);
 
 
 
@@ -14,3 +16,6 @@ INSERT INTO todos(task) VALUES
 ("Demo MYSQL Connection"),
 ("Complete Model"),
 ("Complete Server");
+
+SELECT id, task, completed FROM todos;
+SELECT id, name, task, completed FROM todos WHERE name="nitin";
